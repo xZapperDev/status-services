ddfb68d31484c9736a0facaa17d4a9365509c040
xNyxDevOF <xnyxdev@gmail.com>
2025-06-25 15:01:00 -0400

    Update services.js

--- services.js
+++ services.js
@@ -1,7 +1,5 @@
 const services = [
-    { name: 'API Principal of xNyxDKR', url: 'https://api.xnyxdkr.eu.org', id: 'Api-official' },
-    { name: 'Official Page of xNyxDKR', url: 'https://xnyxdkr.eu.org', id: 'Page-official' },
-    { name: 'Hosting of Page of xNyxDKR', url: 'https://mywebsite-xnyxdkr.onrender.com', id: 'Page-Hosting' }
+    { name: 'API Principal', url: 'https://api.example.org', id: 'Api' }
 ];
 
 module.exports = services; 
